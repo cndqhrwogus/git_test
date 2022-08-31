62c16f262b33f2c62304acb7b2eb1b8a918eb522
cndqhrwogus <cndqhrwogus@naver.com>
2022-08-31 12:05:05 +0100

    USB통신을 통하여 받은 데이터를 넘겨 받은 후 현재 위치와 목적지까지 비교하여 동작하는 기능 추가(version1)

--- curved_detect/motor_control.py
+++ curved_detect/motor_control.py
@@ -5,19 +5,28 @@ import rospy
 import time
 from matplotlib import pyplot as plt , cm, colors
 from std_msgs.msg import String, Float32
-from geometry_msgs.msg import Twist,TransformStamped
-
+from geometry_msgs.msg import Twist,TransformStamped,PoseWithCovarianceStamped
 
 class motor:
-
+    def usb_goal(self,msg):
+        self.dst = PoseWithCovarianceStamped()
+        self.dst.pose.pose.position.x = msg.pose.pose.position.x
+        self.dst.pose.pose.position.y = msg.pose.pose.position.y
     def opencv_callback(self,data):
         self.camera = data.data
+
+    def current_src(self,msg):
+        self.current_source = PoseWithCovarianceStamped()
+        self.current_source.pose.pose.position.x = msg.pose.pose.position.x
+        self.current_source.pose.pose.position.y = msg.pose.pose.position.y
 
     def run(self):
         rospy.init_node('motor_control')
         self.sonar_warning = 'safy'
         self.camera = 'no line'
         global Pub
+        self.goal = rospy.Subscriber('/usb_goal',PoseWithCovarianceStamped,self.usb_goal)
+        self.src = rospy.Subscriber('/amcl_pose',PoseWithCovarianceStamped,self.current_src)
         Pub = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
         Sub = rospy.Subscriber('/lane_detect',String,self.opencv_callback)
         global twist 
@@ -25,27 +34,31 @@ class motor:
         global forwardspeed 
         twist = Twist()
         while not rospy.is_shutdown():
-            if (self.camera == 'Emergency'):
+            if(self.dst == self.current_source):
                 forwardspeed = 0.0
                 anglespeed = 0.0
-            elif (self.camera == 'Straight'):
-                forwardspeed = 0.1
-                anglespeed = 0.0
-            elif (self.camera == 'Right Curve'):
-                anglespeed = -0.3
-                forwardspeed = 0.05
-            elif (self.camera == 'Left Curve'):
-                forwardspeed = 0.05
-                anglespeed = 0.3
-            elif (self.camera == 'no line'):
-                forwardspeed = 0.0
-                anglespeed = 0.0
-            elif (self.camera == 'only left'):
-                forwardspeed = 0.05
-                anglespeed = 0.3
-            elif (self.camera == 'only right'):
-                forwardspeed = 0.05
-                anglespeed = -0.3
+            else:
+                if (self.camera == 'Emergency'):
+                    forwardspeed = 0.0
+                    anglespeed = 0.0
+                elif (self.camera == 'Straight'):
+                    forwardspeed = 0.1
+                    anglespeed = 0.0
+                elif (self.camera == 'Right Curve'):
+                    anglespeed = -0.3
+                    forwardspeed = 0.05
+                elif (self.camera == 'Left Curve'):
+                    forwardspeed = 0.05
+                    anglespeed = 0.3
+                elif (self.camera == 'no line'):
+                    forwardspeed = 0.0
+                    anglespeed = 0.0
+                elif (self.camera == 'only left'):
+                    forwardspeed = 0.05
+                    anglespeed = 0.3
+                elif (self.camera == 'only right'):
+                    forwardspeed = 0.05
+                    anglespeed = -0.3
             twist.angular.z = anglespeed
             twist.linear.x = forwardspeed
             Pub.publish(twist)
