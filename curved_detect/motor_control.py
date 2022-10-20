d1397e477668102a3127999a5dd0a6c1f54de943
Ashe Kim <ashekim@robotis.com>
2022-10-20 19:41:53 +0900

    test

--- curved_detect/motor_control.py
+++ curved_detect/motor_control.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import cv2
 import numpy as np
 import rospy
@@ -19,14 +19,16 @@ class motor:
         self.current_source = PoseWithCovarianceStamped()
         self.current_source.pose.pose.position.x = msg.pose.pose.position.x
         self.current_source.pose.pose.position.y = msg.pose.pose.position.y
-
+    def usb_emergency(self,msg):
+        self.tof_warning = msg.data
     def run(self):
         rospy.init_node('motor_control')
-        self.sonar_warning = 'safy'
+        self.tof_warning = 'safy'
         self.camera = 'no line'
         global Pub
-        self.goal = rospy.Subscriber('/usb_goal',PoseWithCovarianceStamped,self.usb_goal)
+        self.goal = rospy.Subscriber('/usb_cdc',PoseWithCovarianceStamped,self.usb_goal)
         self.src = rospy.Subscriber('/amcl_pose',PoseWithCovarianceStamped,self.current_src)
+        self.emergency = rospy.Subscriber('/emergency',String,self.usb_emergency)
         Pub = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
         Sub = rospy.Subscriber('/lane_detect',String,self.opencv_callback)
         global twist 
@@ -34,34 +36,45 @@ class motor:
         global forwardspeed 
         twist = Twist()
         while not rospy.is_shutdown():
-            if(self.dst == self.current_source):
+            #블루투스 받아서 해당위치까지면 break
+            # try:
+            #     if((self.dst.pose.pose.position.x - 0.1 <= self.current_source.pose.pose.position.x <= self.dst.pose.pose.position.x + 0.1) and (self.dst.pose.pose.position.y - 0.1 <= self.current_source.pose.pose.position.y <= self.dst.pose.pose.position.y + 0.1)):
+            #         print("dst")
+            #         # forwardspeed = 0.0
+            #         # anglespeed = 0.0
+            #     else:
+            #         print("not yet")
+            if (self.tof_warning == 'stop'):
                 forwardspeed = 0.0
                 anglespeed = 0.0
-            else:
-                if (self.camera == 'Emergency'):
-                    forwardspeed = 0.0
+            elif(self.tof_warning == 'safy'):
+                if (self.camera == 'Straight'):
+                    forwardspeed = 0.07
                     anglespeed = 0.0
-                elif (self.camera == 'Straight'):
-                    forwardspeed = 0.1
-                    anglespeed = 0.0
+                    
                 elif (self.camera == 'Right Curve'):
-                    anglespeed = -0.3
+                    anglespeed = -0.5
                     forwardspeed = 0.05
                 elif (self.camera == 'Left Curve'):
                     forwardspeed = 0.05
+                    anglespeed = 0.5
+                elif(self.camera == 'Left Slide'):
+                    forwardspeed = 0.01
                     anglespeed = 0.3
-                elif (self.camera == 'no line'):
+                elif(self.camera == 'Right Slide'):
+                    forwardspeed = 0.01
+                    anglespeed = -0.3
+                else:
                     forwardspeed = 0.0
                     anglespeed = 0.0
-                elif (self.camera == 'only left'):
-                    forwardspeed = 0.05
-                    anglespeed = 0.3
-                elif (self.camera == 'only right'):
-                    forwardspeed = 0.05
-                    anglespeed = -0.3
             twist.angular.z = anglespeed
             twist.linear.x = forwardspeed
+            print(twist)
             Pub.publish(twist)
+            time.sleep(0.3)
+            
+            # except AttributeError:
+            #     print('not')
         rospy.spin()
 
 
